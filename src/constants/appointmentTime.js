8e9adb38ac55bca0d8adf6d13f0c47f711f2bb6c
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2023-05-03 15:01:29 -0300

    feat: create appointTime consts

---
+++ src/constants/appointmentTime.js
@@ -0,0 +1,11 @@
+const appointmentTime = {
+    A: '08:00',
+    B: '09:00',
+    C: '10:00',
+    D: '13:00',
+    E: '14:00',
+    F: '15:00',
+    G: '16:00',
+}
+
+export default appointmentTime
